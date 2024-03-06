7e6d960b4eccf2f6bc39a8fd155b1c51cec4ca3a
Alejandro <alejandrobridoux@hotmail.com>
2024-03-06 00:40:18 -0300

    priemr commit

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+ title: 'SEO Title',
+ description: 'SEO Description',
+ keywords:['Alejandro Bridoux','Curso de NextJS']
+};
+
+
+export default function AboutPage() {
+    return(
+        <>
+            <span className="text-7xl">About Page</span>
+        </>
+    )
+}
---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+ title: 'Contact title',
+ description: 'Contact description',
+ keywords: ['contact', 'React', 'd-_-b']
+};
+
+export default function ContactPage() {
+    return(
+        <>
+        <span className="text-7xl">Contact Page</span>
+        </>
+    )
+}
